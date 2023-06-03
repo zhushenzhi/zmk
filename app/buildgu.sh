852647d2e4d362aff85933f0d58a89d18a2ca97f
gulai22 <gulaizhi@gmail.com>
2023-06-03 19:59:49 +0800

    add charybdis

--- app/buildgu.sh
+++ app/buildgu.sh
@@ -1,8 +1,14 @@
 rm -rf build_left
 rm -rf build_right
 
-west build -d build_left -b nice_nano_v2 -- -DSHIELD=corne_left
-west build -d build_right -b nice_nano_v2 -- -DSHIELD=corne_right
+# west build -d build_left -b nice_nano_v2 -- -DSHIELD=corne_left
+# west build -d build_right -b nice_nano_v2 -- -DSHIELD=corne_right
 
-cp ./build_left/zephyr/zmk.uf2 aaa/zmk_left.uf2 
-cp ./build_right/zephyr/zmk.uf2 aaa/zmk_right.uf2 
+# cp ./build_left/zephyr/zmk.uf2 aaa/zmk_left.uf2 
+# cp ./build_right/zephyr/zmk.uf2 aaa/zmk_right.uf2
+
+west build -d build_left -b nice_nano_v2 -- -DSHIELD=charybdis_left
+cp ./build_left/zephyr/zmk.uf2 aaa/zmk_charybdis_left.uf2 
+
+west build -d build_right -b nice_nano_v2 -- -DSHIELD=charybdis_right
+cp ./build_right/zephyr/zmk.uf2 aaa/zmk_charybdis_right.uf2
